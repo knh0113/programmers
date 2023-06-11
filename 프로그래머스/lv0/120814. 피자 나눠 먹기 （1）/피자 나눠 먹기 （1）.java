7c8fcc4f94f48a1bcad8f989d022eb20599108c6
knh0113 <80758615+knh0113@users.noreply.github.com>
2023-06-11 16:59:51 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i =0; 7*i<n; i++ ) {
+            answer = i+1;
+        } 
+            
+        return answer;
+    }
+}
