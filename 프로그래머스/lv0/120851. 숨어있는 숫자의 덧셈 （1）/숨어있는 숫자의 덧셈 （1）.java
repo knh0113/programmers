819821727f7d43a4f5633155d50e9d2f4ad47e65
knh0113 <80758615+knh0113@users.noreply.github.com>
2023-06-18 19:10:06 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        for(int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if((int)c >= 48 && (int)c <= 57)
+                answer += c-'0';
+        }
+        return answer;
+    }
+}
