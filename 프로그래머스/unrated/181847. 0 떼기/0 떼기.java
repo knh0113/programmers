a7b8c1d31494fb2e17ac5d34f8bb1bfa05226a1c
knh0113 <80758615+knh0113@users.noreply.github.com>
2023-07-26 18:42:36 +0900

    [unrated] Title: 0 떼기, Time: 1.41 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,17 +1,16 @@
 class Solution {
     public String solution(String n_str) {
         String answer = "";
-        String[] str = n_str.split("");
-        for (int i = 0; i < str.length; i++) {
-            if (!str[i].equals("0")) {
-                for (int j = i; j < str.length; j++) {
-                    answer += str[j];
-                  
-                }
-                  break;
+        String[] str = n_str.split("0"); 
+        for (int i =0; i < str.length; i++ ) {
+            if (str[0].contains('0')) {
+                str = str.remove(str[0]);
+                answer = String.valueOf(str);
+                break;
+            } else {
+                answer = String.valueOf(str);
             }
         }
-
         return answer;
     }
 }
